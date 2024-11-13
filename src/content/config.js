1fdea675a6c32e1d2351af7956378e9a77ff9978
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-11-13 05:40:21 -0500

    code linted

--- src/content/config.js
+++ src/content/config.js
@@ -1,15 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
 const videoCollection = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        dateFormatted: z.string(),
-        link: z.string(),
-    }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		dateFormatted: z.string(),
+		link: z.string(),
+	}),
 });
 
 export const collections = {
-    video: videoCollection,
+	video: videoCollection,
 };
