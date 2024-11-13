75452b1d41380cff9737203ee9d5d21273be7d22
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-11-13 05:32:10 -0500

    Portfolio modified updated for Carlos Miguel

--- src/content/config.js
+++ src/content/config.js
@@ -1,14 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
-const postCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		dateFormatted: z.string(),
-	}),
+const videoCollection = defineCollection({
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        dateFormatted: z.string(),
+        link: z.string(),
+    }),
 });
 
 export const collections = {
-	post: postCollection,
+    video: videoCollection,
 };
---
+++ src/content/video/ahorcado.md
@@ -0,0 +1,6 @@
+---
+title: Aprende arrays en JavaScript y crea el juego del ahorcado | Curso de JavaScript desde CERO - Clase 5
+description: Los arrays son uno de los tipos de datos más importantes en cualquier lenguaje de programación ya que nos permiten almacenar múltiples valores en una sola variable. En este video aprenderemos todo lo básico sobre ellos y lo pondremos en práctica creando el juego del ahorcado en la consola del navegador.
+dateFormatted: Oct 15, 2024
+link: "https://youtu.be/jszkUlRv53o?si=Og-sJhxRwiym87ed"
+---
